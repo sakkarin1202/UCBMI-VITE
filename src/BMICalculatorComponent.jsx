f6144340e4d0f9bdea9780f1ef8907330b5a4edf
sakkarin1202 <xfake12340@hotmail.com>
2024-03-21 17:08:16 +0700

    update

--- src/BMICalculatorComponent.jsx
+++ src/BMICalculatorComponent.jsx
@@ -21,7 +21,7 @@ function BMICalculatorComponent() {
 
   return (
     <div>
-      <h1>BMI Calculator</h1>
+      <h1>Sakkarin</h1>
       <div>
         <label>Weight (kg):</label>
         <input
