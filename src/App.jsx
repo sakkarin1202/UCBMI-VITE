c6ad270a343d03a0da2da261f614c77264d916d3
sakkarin1202 <xfake12340@hotmail.com>
2024-03-21 16:58:20 +0700

    update

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+import "./App.css";
+import BMICalculatorComponent from "./BMICalculatorComponent";
+
+function App() {
+  
+
+  return (
+    <>
+      <BMICalculatorComponent />
+    </>
+  );
+}
+
+export default App;
